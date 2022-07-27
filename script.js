a7713648c20423136b41f66310eed37942e793bf
moussatougri <moussatougri23@gmail.com>
2022-07-27 18:21:15 +0200

    Recipe Api fetches

---
+++ script.js
@@ -0,0 +1,4 @@
+//Recipe API
+const appId = "42d120f9";
+const appKeys = "b0dc85059ad7e651a82eb38781cb8389";
+const basicUrl = `https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKeys}`;
