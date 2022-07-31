cfbd8ff803677dee025976ed84f93bb8b0c14dc3
moussatougri <moussatougri23@gmail.com>
2022-07-31 16:38:17 +0200

    get data

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 //Recipe API
 const appId = "42d120f9";
 const appKeys = "b0dc85059ad7e651a82eb38781cb8389";
-const basicUrl = `https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKeys}`;
+
+//selectoren
+const searchBar = document.querySelector(".search-bar");
+const searchBarInput = document.querySelector("input");
+
+//submit input value
+searchBar.addEventListener("submit", function (e) {
+  e.preventDefault();
+  searchBarInput.value;
+  console.log(searchBarInput.value);
+  fetchData(searchBarInput.value);
+  searchBarInput.value = "";
+});
+
+const fetchData = (result) => {
+  const basicUrl = `https://api.edamam.com/search?q=${result}&app_id=${appId}&app_key=${appKeys}&to=20`;
+  fetch(basicUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data.hits);
+    });
+};
