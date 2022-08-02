558c28eefb88f01d77afe04889c49365975df415
moussatougri <moussatougri23@gmail.com>
2022-08-02 20:51:26 +0200

    data fetched individually to create an enlarged detail card

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ searchBar.addEventListener("submit", function (e) {
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   spaceBetween: 5,
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   freeMode: true,
   loopAdditionalSlides: 5,
@@ -49,16 +49,41 @@ const createSilder = (results) => {
   results.forEach(function (result) {
     const swiperSlideDiv = document.createElement("div");
     swiperSlideDiv.classList.add("swiper-slide");
-    const swiperSlideDiv2 = document.createElement("div");
-    const recipeTitle = document.createElement("p");
-    recipeTitle.innerText = result.recipe.label;
-    swiperSlideDiv2.classList.add("tumb-tile");
+    const swiperSlideContainer = document.createElement("div");
+
+    swiperSlideContainer.classList.add("tumb-tile");
+    swiperSlideContainer.setAttribute("data-id", `${result.recipe.label}`);
     const images = document.createElement("img");
     images.src = result.recipe.image;
 
+    const button = document.createElement("button");
+    button.classList.add("view-button");
+    button.innerText = "view more";
+
+    button.addEventListener("click", getMealCard);
+
     swiperWrapper.appendChild(swiperSlideDiv);
-    swiperSlideDiv.appendChild(swiperSlideDiv2);
-    swiperSlideDiv.appendChild(recipeTitle);
-    swiperSlideDiv2.appendChild(images);
+    swiperSlideDiv.appendChild(swiperSlideContainer);
+    swiperSlideContainer.appendChild(button);
+    swiperSlideContainer.appendChild(images);
+    swiperSlideContainer.appendChild(button);
   });
+};
+
+function getMealCard(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("view-button")) {
+    let mealItem = e.target.parentElement;
+
+    const baseUrl = `https://api.edamam.com/search?q=${mealItem.dataset.id}&app_id=${appId}&app_key=${appKeys}&to=1`;
+
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => viewMoreCard(data.hits));
+  }
+}
+
+const viewMoreCard = (data) => {
+  data = data[0].recipe;
+  console.log(data.image);
 };
